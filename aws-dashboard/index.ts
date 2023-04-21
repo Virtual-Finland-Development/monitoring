e56c612ba84d1a159cc4a2d2fd6740be796f0584
lassi.patanen <lassi.patanen@gofore.com>
2023-04-21 09:17:12 +0300

    Created initial CloudWatch dashboard and workflow for publishing dashboard

---
+++ aws-dashboard/index.ts
@@ -0,0 +1,23 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+const stack = pulumi.getStack();
+const projectName = pulumi.getProject();
+
+const dashboard = new aws.cloudwatch.Dashboard(`${projectName}-${stack}`, {
+  dashboardName: `${projectName}-${stack}`,
+  dashboardBody: JSON.stringify({
+    widgets: [
+      {
+        type: "text",
+        x: 0,
+        y: 7,
+        width: 3,
+        height: 3,
+        properties: {
+          markdown: "Users API dashboard",
+        },
+      }
+    ]
+  })
+});
