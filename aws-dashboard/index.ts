5c40384b52539ed34cad09855c57ee4ea577c18d
lassi.patanen <lassi.patanen@gofore.com>
2023-04-22 08:51:16 +0300

    Add database connection gauge widget for Users API database

--- aws-dashboard/index.ts
+++ aws-dashboard/index.ts
@@ -4,6 +4,11 @@ import {DashboardTitle, DashboardTitleSize} from "./templates/dashboardTitle";
 
 const stack = pulumi.getStack();
 const projectName = pulumi.getProject();
+const config = new pulumi.Config();
+const org: string = config.require('org');
+
+// Stack references
+const usersApiDb = new pulumi.StackReference(`${org}/users-api-postgres-db/${stack}`);
 
 // noinspection JSUnusedLocalSymbols
 const dashboard = new aws.cloudwatch.Dashboard(`${projectName}-${stack}`, {
@@ -11,7 +16,30 @@ const dashboard = new aws.cloudwatch.Dashboard(`${projectName}-${stack}`, {
   dashboardBody: JSON.stringify({
     widgets: [
       new DashboardTitle().withBody("Useful graphs and metrics for monitoring Virtual Finland services").create("Virtual Finland Development dashboard", 0, 0),
-      new DashboardTitle().create("Users API stats", 0, 10, DashboardTitleSize.MEDIUM)
+      new DashboardTitle().create("Users API stats", 0, 10, DashboardTitleSize.MEDIUM),
+      {
+        "height": 5,
+        "width": 4,
+        "y": 2,
+        "x": 0,
+        "type": "metric",
+        "properties": {
+          "view": "gauge",
+          "metrics": [
+            [ "AWS/RDS", "DatabaseConnections", "DBInstanceIdentifier", usersApiDb.name ]
+          ],
+          "region": config.require('region'),
+          "setPeriodToTimeRange": false,
+          "sparkline": true,
+          "trend": true,
+          "yAxis": {
+            "left": {
+              "min": 0,
+              "max": 100
+            }
+          }
+        }
+      }
     ]
   })
 });
