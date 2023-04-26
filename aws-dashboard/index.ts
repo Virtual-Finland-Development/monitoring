59058016228feee1df1ed3b4a8929950a362cd97
lassi.patanen <lassi.patanen@gofore.com>
2023-04-26 09:20:54 +0300

    Purkka to make demo work

--- aws-dashboard/index.ts
+++ aws-dashboard/index.ts
@@ -10,38 +10,37 @@ const org: string = config.require('org');
 // Stack references
 const usersApiDb = new pulumi.StackReference(`${org}/users-api/${stack}`);
 
-const usersApiDbIdentifier = usersApiDb.getOutputDetails('DbIdentifier');
-
 // noinspection JSUnusedLocalSymbols
 const dashboard = new aws.cloudwatch.Dashboard(`${projectName}-${stack}`, {
   dashboardName: `${projectName}-${stack}`,
-  dashboardBody: JSON.stringify({
-    widgets: [
-      new DashboardTitle().withBody("Useful graphs and metrics for monitoring Virtual Finland services").create("Virtual Finland Development dashboard", 0, 0),
-      new DashboardTitle().create("Users API stats", 0, 10, DashboardTitleSize.MEDIUM),
-      {
-        "height": 5,
-        "width": 4,
-        "y": 2,
-        "x": 0,
-        "type": "metric",
-        "properties": {
-          "view": "gauge",
-          "metrics": [
-            [ "AWS/RDS", "DatabaseConnections", "DBInstanceIdentifier",  pulumi.interpolate`${usersApiDbIdentifier}`]
-          ],
-          "region": config.require('region'),
-          "setPeriodToTimeRange": false,
-          "sparkline": true,
-          "trend": true,
-          "yAxis": {
-            "left": {
-              "min": 0,
-              "max": 100
+  dashboardBody: usersApiDb.getOutput('DbIdentifier').apply(DbIdentifier =>
+    JSON.stringify({
+      widgets: [
+        new DashboardTitle().withBody("Useful graphs and metrics for monitoring Virtual Finland services").create("Virtual Finland Development dashboard", 0, 0),
+        new DashboardTitle().create("Users API stats", 0, 10, DashboardTitleSize.MEDIUM),
+        {
+          "height": 5,
+          "width": 4,
+          "y": 2,
+          "x": 0,
+          "type": "metric",
+          "properties": {
+            "view": "gauge",
+            "metrics": [
+              ["AWS/RDS", "DatabaseConnections", "DBInstanceIdentifier", DbIdentifier]
+            ],
+            "region": config.require('region'),
+            "setPeriodToTimeRange": false,
+            "sparkline": true,
+            "trend": true,
+            "yAxis": {
+              "left": {
+                "min": 0,
+                "max": 100
+              }
             }
           }
         }
-      }
-    ]
-  })
+      ]
+    }))
 });
