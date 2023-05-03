23bdd7190b9c902e19bb6574f74c053d216c613f
lassi.patanen <lassi.patanen@gofore.com>
2023-05-03 15:18:00 +0300

    Added lambda at edge invocations widget for codesets API

--- aws-dashboard/index.ts
+++ aws-dashboard/index.ts
@@ -15,13 +15,21 @@ const usersApiLambdaId = usersApiStackReference.getOutput('LambdaId').apply(v =>
 const testbedApiStackReference = new pulumi.StackReference(`${org}/testbed-api/${stack}`);
 const testbedApiLambdaId = testbedApiStackReference.getOutput('LambdaId').apply(v => v.toString());
 
+const codesetsStackReference = new pulumi.StackReference(`${org}/codesets/${stack}`);
+const codesetsLambdaId = codesetsStackReference.getOutput('lambdaId').apply(v => v.toString());
+
 // Combine all resources to single output that we can use below on the dashboard
-let resources = pulumi.all([usersApiDbInstanceIdentifier, usersApiLambdaId, testbedApiLambdaId]);
+let resources = pulumi.all([
+  usersApiDbInstanceIdentifier,
+  usersApiLambdaId,
+  testbedApiLambdaId,
+  codesetsLambdaId
+]);
 
 // noinspection JSUnusedLocalSymbols
 const dashboard = new aws.cloudwatch.Dashboard(`${projectName}-${stack}`, {
   dashboardName: `${projectName}-${stack}`,
-  dashboardBody: resources.apply(([usersApiDbInstanceIdentifier, usersApiLambdaId, testbedApiLambdaId]) =>
+  dashboardBody: resources.apply(([usersApiDbInstanceIdentifier, usersApiLambdaId, testbedApiLambdaId, codesetsLambdaId]) =>
     JSON.stringify({
       widgets: [
         new DashboardTitle().withBody("Useful graphs and metrics for monitoring Virtual Finland services").create("Virtual Finland Development dashboard", 0, 0),
@@ -83,6 +91,45 @@ const dashboard = new aws.cloudwatch.Dashboard(`${projectName}-${stack}`, {
             "region": "eu-north-1",
             "stat": "Minimum",
             "period": 300
+          }
+        },
+        {
+          "type": "metric",
+          "x": 0,
+          "y": 24,
+          "width": 24,
+          "height": 12,
+          "properties": {
+            "view": "timeSeries",
+            "stacked": false,
+            "metrics": [
+              [ "AWS/Lambda", "Invocations", "FunctionName", codesetsLambdaId.split(":")[0], "Resource", codesetsLambdaId, { "id": "m1", "visible": true, "label": "US-East (N. Virginia)" } ],
+              [ "...", { "region": "us-east-2", "id": "m2", "visible": true, "label": "US-East (Ohio)" } ],
+              [ "...", { "region": "us-west-1", "id": "m3", "visible": true, "label": "US-West (N. California)" } ],
+              [ "...", { "region": "us-west-2", "id": "m4", "visible": true, "label": "US-West (Oregon)" } ],
+              [ "...", { "region": "ap-south-1", "id": "m5", "visible": true, "label": "Asia Pacific (Mumbai)" } ],
+              [ "...", { "region": "ap-northeast-1", "id": "m6", "visible": true, "label": "Asia Pacific (Tokyo)" } ],
+              [ "...", { "region": "ap-northeast-2", "id": "m7", "visible": true, "label": "Asia Pacific (Seoul)" } ],
+              [ "...", { "region": "ap-southeast-1", "id": "m8", "visible": true, "label": "Asia Pacific (Singapore)" } ],
+              [ "...", { "region": "ap-southeast-2", "id": "m9", "visible": true, "label": "Asia Pacific (Sydney)" } ],
+              [ "...", { "region": "eu-west-1", "id": "m10", "visible": true, "label": "EU (Ireland)" } ],
+              [ "...", { "region": "eu-west-2", "id": "m11", "visible": true, "label": "EU (London)" } ],
+              [ "...", { "region": "eu-central-1", "id": "m12", "visible": true, "label": "EU (Frankfurt)" } ],
+              [ "...", { "region": "sa-east-1", "id": "m13", "visible": true, "label": "South America (Sao Paulo)" } ],
+              [ { "expression": "(m1+m2+m3+m4+m5+m6+m7+m8+m9+m10+m11+m12+m13)", "label": "Global (sum)" } ]
+            ],
+            "region": "us-east-1",
+            "title": "Invocations (sum)",
+            "period": 300,
+            "yAxis": {
+              "left": {
+                "showUnits": false
+              },
+              "right": {
+                "showUnits": false
+              }
+            },
+            "stat": "Sum"
           }
         },
       ]
