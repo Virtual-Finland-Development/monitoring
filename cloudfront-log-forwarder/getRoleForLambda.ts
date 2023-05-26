fc8764fb70884420d06e343029326ddb90d1871a
lassi.patanen <lassi.patanen@gofore.com>
2023-05-26 09:12:27 +0300

    Infra for deploying log forwarder

---
+++ cloudfront-log-forwarder/getRoleForLambda.ts
@@ -0,0 +1,67 @@
+import * as aws from "@pulumi/aws";
+import {ManagedPolicies} from "@pulumi/aws/iam";
+import AWSLambdaBasicExecutionRole = ManagedPolicies.AWSLambdaBasicExecutionRole;
+
+export function getRoleForLambda(projectName: string, stack: string, tags: any): aws.iam.Role {
+
+    const assumeRolePolicy = aws.iam.getPolicyDocument({
+        statements: [
+            {
+                effect: 'Allow',
+                principals: [{type: 'Service', identifiers: ['lambda.amazonaws.com']}],
+                actions: ['sts:AssumeRole']
+            }]
+    });
+
+    const role = new aws.iam.Role(`${projectName}-lambda-role-${stack}`, {
+        assumeRolePolicy: assumeRolePolicy.then(assumeRole => assumeRole.json),
+        tags: tags
+    })
+
+    new aws.iam.RolePolicyAttachment(`${projectName}-lambdaBasicExecutionRolePolicy-${stack}`, {
+        role: role,
+        policyArn: AWSLambdaBasicExecutionRole
+    });
+
+    new aws.iam.RolePolicy(`${projectName}-allowLambdaAccessToS3AndCloudWatch-${stack}`, {
+        role: role,
+        policy: JSON.stringify({
+            Version: '2012-10-17',
+            Statement: [
+                {
+                    Effect: 'Allow',
+                    Principals: [
+                        {
+                            type: 'Service',
+                            identifiers: ['s3.amazonaws.com']
+                        }
+                    ],
+                    Actions: [
+                        's3:ListObject',
+                        's3:GetObject'
+                    ]
+                },
+                {
+                    Effect: 'Allow',
+                    Principals: [
+                        {
+                            type: 'Service',
+                            identifiers: ['cloudwatch.amazonaws.com']
+                        }
+                    ],
+                    Actions: [
+                        'logs:CreateLogGroup',
+                        'logs:CreateLogStream',
+                        'logs:DescribeLogGroups',
+                        'logs:DescribeLogStreams',
+                        'logs:PutLogEvents',
+                        'logs:GetLogEvents',
+                        'logs:FilterLogEvents'
+                    ]
+                }
+            ]
+        })
+    })
+
+    return role;
+}
---
+++ cloudfront-log-forwarder/index.ts
@@ -0,0 +1,68 @@
+
+import * as pulumi from '@pulumi/pulumi';
+import * as aws from '@pulumi/aws';
+import {getRoleForLambda} from "./getRoleForLambda";
+
+// General setup
+const stack = pulumi.getStack();
+const projectName = pulumi.getProject();
+const config = new pulumi.Config();
+let artifactPath = config.get('artifactPath') ?? 'src/bin/Release/net6.0/src.zip'
+
+let tags = {
+    'vfd:stack': stack,
+    'vfd:project': projectName,
+};
+
+
+let iamForLambda = getRoleForLambda(projectName, stack, tags);
+
+// Create Lambda function for forwarding logs 
+const lambdaFunction = new aws.lambda.Function(`${projectName}-${stack}`, {
+    code: new pulumi.asset.FileArchive(artifactPath),
+    role: iamForLambda.arn,
+    handler: 'cloudfront-log-forwarder::CloudFrontLogForwarder.Functions::FunctionHandler',
+    runtime: 'dotnet6',
+    environment: {
+        variables: {
+            logGroupName: `/aws/cloudfront/${projectName}-${stack}`,
+            logStreamName: `${projectName}-log-stream-${stack}`
+        }
+    },
+    memorySize: 128,
+    timeout: 10,
+    tags: tags
+});
+
+let org: string = 'virtualfinland';
+const codesetsStackReference = new pulumi.StackReference(`${org}/codesets/${stack}`);
+const standardLogsBucketDetails = codesetsStackReference.getOutput('standardLogsBucketDetails');
+
+// Create Role and policies required by Lambda function
+const lambdaBucketPermission = new aws.lambda.Permission(`${projectName}-lambda-permission-${stack}`, {
+    action: 'lambda:InvokeFunction',
+    function: lambdaFunction.arn,
+    principal: 's3.amazonaws.com',
+    sourceArn: standardLogsBucketDetails.apply(o => o.arn.toString())
+}, {
+    dependsOn: [codesetsStackReference]
+})
+
+new aws.s3.BucketNotification(`${projectName}-notification-${stack}`, {
+    bucket: standardLogsBucketDetails.apply(o => o.id.toString()),
+    lambdaFunctions: [{
+        lambdaFunctionArn: lambdaFunction.arn,
+        events: ['s3:ObjectCreated:Put'],
+        filterSuffix: '.gzip'
+    }]
+}, {
+    dependsOn: [lambdaBucketPermission, codesetsStackReference]
+});
+
+// Create CloudWatch log group where logs will be saved
+const logGroup = new aws.cloudwatch.LogGroup(`/aws/cloudfront/${projectName}-${stack}`, {
+    tags: tags
+})
+
+// noinspection JSUnusedGlobalSymbols
+export const logGroupName = logGroup.name;
