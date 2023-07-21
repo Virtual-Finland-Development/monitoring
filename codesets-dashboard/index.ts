e1f033fbf95d001ef6988959e95b99dfde2b2628
Lassi Piironen <lsipii@kapsi.fi>
2023-07-21 17:33:21 +0300

    style: fix cols/rows

--- codesets-dashboard/index.ts
+++ codesets-dashboard/index.ts
@@ -1,6 +1,5 @@
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
-import { DashboardTitle, DashboardTitleSize } from "../aws-dashboard/templates/dashboardTitle";
 
 const stack = pulumi.getStack();
 const projectName = pulumi.getProject();
@@ -158,38 +157,48 @@ const dashboard = new aws.cloudwatch.Dashboard(`${projectName}-${stack}`, {
           view: "pie"
         }
       },
-      new DashboardTitle().create("ESCO API stats", 0, 10, DashboardTitleSize.MEDIUM),
       {
-        "height": 8,
-        "width": 15,
-        "y": 15,
-        "x": 0,
-        "type": "log",
-        "properties": {
-          "query": `SOURCE '/aws/lambda/${escoApiLambdaId}' | fields @timestamp, @@x, StatusCode, Elapsed\n| filter StatusCode = 500\n| sort @timestamp desc\n| limit 20`,
-          "region": region,
-          "stacked": false,
-          "title": "ESCO API errors",
-          "view": "table"
+        type: 'text',
+        x: 0,
+        y: 24,
+        width: 15,
+        height: 2,
+        properties: {
+          markdown: '## ESCO API stats',
+          background: 'transparent'
         }
       },
       {
-        "height": 6,
-        "width": 7,
-        "y": 2,
-        "x": 7,
-        "type": "metric",
-        "properties": {
-          "metrics": [
+        height: 6,
+        width: 8,
+        y: 26,
+        x: 0,
+        type: 'log',
+        properties: {
+          query: `SOURCE '/aws/lambda/${escoApiLambdaId}' | fields @timestamp, @@x, StatusCode, Elapsed\n| filter StatusCode = 500\n| sort @timestamp desc\n| limit 20`,
+          region: region,
+          stacked: false,
+          title: 'ESCO API errors',
+          view: 'table'
+        }
+      },
+      {
+        height: 6,
+        width: 8,
+        y: 26,
+        x: 8,
+        type: 'metric',
+        properties: {
+          metrics: [
             [ "AWS/Lambda", "Duration", "FunctionName", escoApiLambdaId, { "id": "m1", "region": region, "color": "#3e82e5", "label": "ESCO API (Avg)", "stat": "Average" } ],
             [ "...", { "id": "m2", "region": region, "color": "#8cc8f3", "label": "ESCO API (Min)" } ],
             [ "...", { "id": "m3", "region": region, "color": "#38549a", "label": "ESCO API (Max)", "stat": "Maximum" } ],
           ],
-          "view": "timeSeries",
-          "stacked": false,
-          "region": region,
-          "stat": "Minimum",
-          "period": 300
+          view: 'timeSeries',
+          stacked: false,
+          region: region,
+          stat: 'Minimum',
+          period: 300
         }
       }
     ]
