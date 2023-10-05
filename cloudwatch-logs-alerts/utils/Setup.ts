282bb7d47fb3a765ef06682f630670aad4434a13
Lauri Saarela <lauri.saarela@gofore.com>
2023-10-05 14:10:03 +0300

    setup: remove unecessary stuff

--- cloudwatch-logs-alerts/utils/Setup.ts
+++ cloudwatch-logs-alerts/utils/Setup.ts
@@ -1,4 +1,3 @@
-import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
 
 const stage = pulumi.getStack();
@@ -23,11 +22,6 @@ function isProductionLikeEnvironment() {
   return stage.endsWith("production") || stage.endsWith("staging");
 }
 
-const resourcesRegion = new pulumi.Config("aws").require("region");
-const resourcesRegionProvider = new aws.Provider(`region-${resourcesRegion}`, {
-  region: resourcesRegion as pulumi.Input<aws.Region> | undefined,
-});
-
 const setup = {
   stage,
   projectName,
@@ -35,15 +29,10 @@ const setup = {
   getResourceConfig,
   getResourceName,
   isProductionLikeEnvironment,
-  regions: {
-    resourcesRegion: {
-      name: resourcesRegion,
-      provider: resourcesRegionProvider,
-    },
-  },
 };
 
 type ISetup = typeof setup;
+
 function getSetup() {
   return setup;
 }
