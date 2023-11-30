ee2838063f6949986c8dae39ff9c02842905dd3c
Lassi Piironen <lsipii@kapsi.fi>
2023-11-30 14:01:14 +0200

    fix: skip deploying chatbot if not configured

--- cloudwatch-logs-alerts/resources/Chatbot.ts
+++ cloudwatch-logs-alerts/resources/Chatbot.ts
@@ -1,14 +1,19 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsNative from "@pulumi/aws-native";
+import * as pulumi from "@pulumi/pulumi";
 import { ISetup } from "../utils/Setup";
 
 const config = new pulumi.Config();
 
-export function createChatbotSlackConfig(
-  setup: ISetup,
-  snsTopic: aws.sns.Topic
-) {
+export function createChatbotSlackConfig(setup: ISetup, snsTopic: aws.sns.Topic) {
+  const slackChannelId = config.get("slackChannelId");
+  const slackWorkspaceId = config.get("slackWorkspaceId");
+
+  if (!slackChannelId || !slackWorkspaceId) {
+    console.log("Skipping Slack configuration as slackChannelId or slackWorkspaceId is not set");
+    return;
+  }
+
   // Create an IAM role for Chatbot configuration
   const chatbotRole = new aws.iam.Role(setup.getResourceName("ChatBotRole"), {
     description: "IAM role for AWS Chatbot",
@@ -26,18 +31,15 @@ export function createChatbotSlackConfig(
     }),
   });
 
-  const slackChannelConfig = new awsNative.chatbot.SlackChannelConfiguration(
-    setup.getResourceName("SlackChannelConfig"),
-    {
-      configurationName: "SlackChannelAlertsConfig",
-      iamRoleArn: chatbotRole.arn,
-      slackChannelId: config.require("slackChannelId"),
-      slackWorkspaceId: config.require("slackWorkspaceId"),
-      snsTopicArns: [snsTopic.arn],
-      loggingLevel: "ERROR",
-      userRoleRequired: false,
-    }
-  );
+  const slackChannelConfig = new awsNative.chatbot.SlackChannelConfiguration(setup.getResourceName("SlackChannelConfig"), {
+    configurationName: "SlackChannelAlertsConfig",
+    iamRoleArn: chatbotRole.arn,
+    slackChannelId,
+    slackWorkspaceId,
+    snsTopicArns: [snsTopic.arn],
+    loggingLevel: "ERROR",
+    userRoleRequired: false,
+  });
 
   return slackChannelConfig;
 }
